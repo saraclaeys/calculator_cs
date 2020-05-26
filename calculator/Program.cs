920f7d97a4e664d129913716f0d2c01c5041dc9a
saraclaeys <sara.claeys01@gmail.com>
2020-05-26 12:26:37 +0200

    operators

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -8,10 +8,32 @@ namespace calculator
         {
             Console.Write("Enter a number: ");
             double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operator: ");
+            string op = Console.ReadLine();
+
             Console.Write("Enter another number: ");
             double num2 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine(num1 + num2);
+            if (op == "+")
+            {
+                Console.Write(num1 + num2);
+
+            } else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            } else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
 
             Console.ReadLine();
         }
